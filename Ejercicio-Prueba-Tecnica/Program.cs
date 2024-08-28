e5bc0b399ad18992737fd92c3d3394689d025717
matiasgandolficrombie <160607890+matiasgandolficrombie@users.noreply.github.com>
2024-08-28 18:26:11 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio-Prueba-Tecnica/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Ejercicio_Prueba_Tecnica;
+
+var servicio = new Servicio();
+var razasGatos = await servicio.GetAllCatBreed();
+
+if(razasGatos != null)
+{
+    var primerasDiez = razasGatos.OrderByDescending(p => p.intelligence).Take(10).ToList();
+
+
+
+    foreach (var razas in primerasDiez)
+    {
+        Console.WriteLine("Nombre: "+ razas.name);
+        Console.WriteLine("Descripcion: "+ razas.description);
+        Console.WriteLine("Origen"+ razas.origin);
+        Console.WriteLine("Inteligencia: "+ razas.intelligence);
+        Console.WriteLine("Adaptabilidad: "+ razas.adaptability);
+    }
+
+    double promedioAdaptabilidad = razasGatos.Average(p => p.adaptability);
+    Console.WriteLine("El promedio de adaptabilidad es "+ promedioAdaptabilidad);
+}
+else
+{
+    Console.WriteLine("No se pudo obtener informacion");
+}
+
+
---
+++ Ejercicio-Prueba-Tecnica/Servicio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Prueba_Tecnica
+{
+    public class Servicio
+    {
+        //Get: lista de razas de gatos
+        //https://api.thecatapi.com/v1/breeds
+
+        public async Task<List<CatBreed>> GetAllCatBreed()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://api.thecatapi.com/v1/breeds");
+                HttpResponseMessage res = await client.GetAsync("breeds");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    string json = await res.Content.ReadAsStringAsync();
+                    var respuesDes = JsonSerializer.Deserialize<List<CatBreed>>(json);
+                    return respuesDes;
+
+                }
+                else
+                {
+                    Console.WriteLine("El error es :" + res.StatusCode);
+                    return null;
+                }
+
+
+
+            }
+        }
+    }
+}
